e3fe72faa75609871c5552ab26778be3e1dabcc5
AlexKaracaoglu <karacaog@bc.edu>
2019-05-28 11:12:10 -0400

    added BaseEntity. Closes #10

--- pet-clinic-data/src/main/java/learn/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/learn/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package learn.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/learn/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/learn/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package learn.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
