7f6d234231003cdfcaea53a7995bd28ba9490ea2
AlexKaracaoglu <karacaog@bc.edu>
2019-05-28 14:46:41 -0400

    owners page and controller added. Closes #13

--- pet-clinic-web/src/main/java/learn/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/learn/springpetclinic/controllers/OwnerController.java
@@ -3,13 +3,13 @@ package learn.springpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
-        return "vets/index";
+    public String listOwners() {
+        return "owners/index";
     }
 
 }
