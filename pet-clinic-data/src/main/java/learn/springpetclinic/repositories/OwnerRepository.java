aafc9c9c242b1309052e9036075e58c99e73dbea
AlexKaracaoglu <karacaog@bc.edu>
2019-06-03 21:08:54 -0400

    Added ability to find an owner. Closes #54

--- pet-clinic-data/src/main/java/learn/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/learn/springpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,12 @@ package learn.springpetclinic.repositories;
 import learn.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
