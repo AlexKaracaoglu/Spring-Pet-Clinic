e8da9f8afa74370645abcd48677e1fc5e4cee06f
AlexKaracaoglu <karacaog@bc.edu>
2019-06-03 21:39:38 -0400

    Added ability to create or edit an owner. Closes #55

--- pet-clinic-web/src/main/java/learn/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/learn/springpetclinic/controllers/OwnerController.java
@@ -8,9 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -65,6 +67,43 @@ public class OwnerController {
         ModelAndView mav = new ModelAndView("owners/ownerDetails");
         mav.addObject("owner", ownerService.findById(ownerId));
         return mav;
+    }
+
+    @GetMapping("/owners/new")
+    public String createOrUpdateOwner(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/owners/new")
+    public String processCreationForm(@Valid Owner owner, BindingResult result) {
+        if (result.hasErrors()) {
+            return "owners/createOrUpdateOwnerForm";
+        }
+        else {
+            Owner savedOwner = ownerService.save(owner);
+            return "redirect:/owners/" + savedOwner.getId();
+        }
+    }
+
+    @GetMapping("/owners/{ownerId}/edit")
+    public String initUpdateForm(@PathVariable("ownerId") Long ownerId, Model model) {
+        Owner owner = ownerService.findById(ownerId);
+        model.addAttribute("owner", owner);
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/owners/{ownerId}/edit")
+    public String processUpdateOwnerForm(@Valid Owner owner, BindingResult result,
+                                         @PathVariable("ownerId") Long ownerId) {
+        if (result.hasErrors()) {
+            return "owners/createOrUpdateOwnerForm";
+        }
+        else {
+            owner.setId(ownerId);
+            ownerService.save(owner);
+            return "redirect:/owners/{ownerId}";
+        }
     }
 
 //    @GetMapping("/owners/{ownerId}")
